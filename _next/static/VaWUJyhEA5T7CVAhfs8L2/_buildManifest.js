413c036a39c920c128b5ec40df9794c1f58140c7
dongkeuny50 <dongkeuny50@users.noreply.github.com>
2022-06-09 09:09:16 +0000

    deploy: 3531a096143407e262e6eba470e5eb8e26bc2d3a

--- _next/static/VaWUJyhEA5T7CVAhfs8L2/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/915-3539dc5c47f0c97f.js","static/chunks/pages/index-45286b12fce47b4c.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
